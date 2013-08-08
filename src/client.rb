033f02677f940bccd61527e14cbaa1f2def71aad
Anz_MSI <pjc0247@naver.com>
2013-08-08 14:34:11 +0900

    recv, send 프로토타�

--- src/client.rb
+++ src/client.rb
@@ -1,4 +1,5 @@
 require 'socket'
+load 'packet.rb'
 
 begin
   c = TCPSocket.new "localhost", 9919
@@ -6,4 +7,40 @@ rescue
   puts "unable to connect"
 end
 
-puts c.read(4)
+
+def recv(socket)
+	p = Packet.new
+
+	p.type = socket.read(4).ord
+	p.timestamp = socket.read(4).ord
+	size = socket.read(4).ord
+
+	for i in 0..size-1
+		name = socket.read(16)
+		size = socket.read(4).ord
+		data = socket.read(size)
+
+		d = Data.new(name, size, data)
+
+		p.data[name] = d
+	end
+
+	return p
+end
+
+def send(socket, packet)
+	header = [packet.type, packet.timestamp, packet.data.size].pack('i*')
+
+	socket.send header, 0
+end
+
+while true
+	p = recv(c)	
+
+	case p.type
+		when 100
+			send c, nil
+	end
+end
+
+c.close
