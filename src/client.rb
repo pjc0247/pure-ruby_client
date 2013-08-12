29f51c214ac77a96c87c751cfd22b2cf41b19759
Anz_MSI <pjc0247@naver.com>
2013-08-12 13:33:03 +0900

    ㅇㅇ

--- src/client.rb
+++ src/client.rb
@@ -9,18 +9,18 @@ end
 
 
 def recv(socket)
-	p = Packet.new
+	p = NetPacket.new
 
-	p.type = socket.read(4).ord
-	p.timestamp = socket.read(4).ord
-	size = socket.read(4).ord
+	p.type			= socket.read(4).unpack('i')[0]
+	p.timestamp		= socket.read(4).unpack('i')[0]
+	size			= socket.read(4).unpack('i')[0]
 
 	for i in 0..size-1
-		name = socket.read(16)
-		size = socket.read(4).ord
-		data = socket.read(size)
+		name = socket.read(16).unpack('a16')[0]
+		size = socket.read(4).unpack('i')[0]
+		data = socket.read(size).unpack('i')[0]
 
-		d = Data.new(name, size, data)
+		d = NetData.new(data, size)
 
 		p.data[name] = d
 	end
@@ -32,14 +32,39 @@ def send(socket, packet)
 	header = [packet.type, packet.timestamp, packet.data.size].pack('i*')
 
 	socket.send header, 0
+
+	packet.data.each do |name, datum|
+		socket.send [name].pack('a16'), 0
+		socket.send [datum.size].pack('i'), 0
+
+		puts datum.size
+
+		case datum.data.class.name
+			when "String"
+				socket.send [datum.data].pack('a' + (datum.data.size+1).to_s), 0
+			when "Fixnum"
+				socket.send [datum.data].pack('i'), 0
+			when "Float"
+				socket.send [datum.data].pack('f'), 0
+		end
+	end
 end
+
+p = NetPacket.new(300)
+p.timestamp = 0
+p.data["id"] = NetData.new("pjc0247")
+p.data["pw"] = NetData.new("asdf")
+
+send(c, p)
 
 while true
 	p = recv(c)	
 
+	puts "recv " + p.type.to_s
+
 	case p.type
 		when 100
-			send c, nil
+#			send c, nil
 	end
 end
 
